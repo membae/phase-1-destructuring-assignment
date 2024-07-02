86feb2eed63a38e8b85f8b40e168e9f6d2141c88
membae <membaphilip1@gmail.com>
2024-07-02 12:56:28 +0300

    added

--- index.js
+++ index.js
@@ -28,21 +28,56 @@ const nestedMuppet = {
 // Strings
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
 
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
+const [bessie, , dolly, babe, little] = farmAnimals.split(' ');
 
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
+
+
+// Split the farmAnimals string into an array and remove the horse and chicken
+const [cow, , sheep, pig] = farmAnimals.split(' ');
+
+// Destructure the remaining animals to assign to the color variables
+const [blackAndWhite, black, pink] = [cow, sheep, pig];
+
+console.log(blackAndWhite); // cow
+console.log(black);         // sheep
+console.log(pink);          // pig
 
 // Arrays
 
 // 4. Use destructuring to assign appropriate variables using the color names.
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
 
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
+const [r, o, y, g, b, , v] = colors;
 
 // 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
+const [, , , , , indg] = colors;
+
 
 // Objects
 
 // 7. Use destructuring to assign all variables using the keys as the variable names
+const { muppetName, color, song, job, partner } = muppet;
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+
+// Destructure to get song2, song4, nestedJob, and nestedPartner
+const {
+  album: {
+    theMuppetMovie: {
+      song2,
+      song4
+    }
+  },
+  nestedJob,
+  nestedPartner
+} = nestedMuppet;
+
+console.log(song2);        // Moving Right Along
+console.log(song4);        // I Hope That Something Better Comes Along
+console.log(nestedJob);    // Host of The Muppet Show
+console.log(nestedPartner); // Miss Piggy
